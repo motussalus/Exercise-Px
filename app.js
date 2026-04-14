8a43032ffdccb75a663c22b83f20bbc3d34c4edf
motussalus <motussalus@gmail.com>
2026-04-14 07:32:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -329,7 +329,8 @@
                 </div>
   
                 <p class="radar-caption">
-                  The goal is not to maximize every specifier equally. The goal is to show, transparently, what the exercise intervention is trying to accomplish and which variables matter most for that case. As seen in this radar diagram example, current literature suggests that different disorders can vary in specifier priorities. As noted here, the difference in ADHD and Eating Disorders is the role of clinician or specialist oversight and involvement.
+                  The 7 Specifiers capture the core domains needed to specify, interpret, and compare exercise interventions across disorders and diagnoses with greater fidelity, reproducibility, and therapeutic precision.<br><br>
+                  As seen in this radar diagram example, current literature suggests that a key difference between ADHD and eating disorders is the role and need for clinician or specialist oversight and involvement. The goal is not to maximize every specifier equally, but to identify which specifiers deserve greater or lesser priority in shaping clinically meaningful outcomes. <em>See the Data &amp; Sources page for a brief overview of each specifier.</em>
                 </p>
               </div>
             </div>
