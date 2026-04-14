4ca0ae1664c7e6fdac0fe51af321096c1f9f3314
motussalus <motussalus@gmail.com>
2026-04-14 07:00:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -281,7 +281,7 @@
               
               <p class="hero-sub">
                 These seven specifiers are key categories of exercise prescription to note, prioritize, and control in order to improve replicability, clarity, and clinical outcomes.<br>
-                Use the Dose + Plan page to see how each specifier is unique and can be translated into a practical prescription structure.
+                Use the Data & Sources page to see a breif overview of each specifier and an example of its necessity in clinical use.
               </p>
               
               <div class="hero-actions hero-actions-four">
